72cd72f80f7d7d5514c89c95fee5c94c9615c69f
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-07-22 15:40:35 +0200

    Estructura inicial de carpetas

---
+++ src/interfaces/Room.ts
@@ -0,0 +1,12 @@
+export interface Room {
+    id: number,
+    fotoLink: string,
+    number: string,
+    floor: number,
+    bedType: string,
+    amenities: string,
+    price: number,
+    status: boolean,
+    offer: number
+    cancelPolicy: string
+}
---
+++ src/interfaces/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: number,
+    photo: string,
+    name: string,
+    startDate: string,
+    email: string,
+    job: string,
+    phone: string,
+    status: boolean,
+    pass: string
+}
